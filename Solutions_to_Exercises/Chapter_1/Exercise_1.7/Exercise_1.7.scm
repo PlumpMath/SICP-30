6bc25ed31643c1881e58a013f550ae5bbf851115
Lawrence R. Amlord <informlarry@gmail.com>
2013-04-03 01:38:49 +0800

    Modify Exercise_1.7.scm.
    Thank you!
    						Lawrence R. Amlord
    						   Apr. 3, 2013
    						   Xi'an, China

--- Solutions_to_Exercises/Chapter_1/Exercise_1.7/Exercise_1.7.scm
+++ Solutions_to_Exercises/Chapter_1/Exercise_1.7/Exercise_1.7.scm
@@ -1,14 +1,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;                  SICP Exercise 1.7 Implementation
+;;             Author: Lawrence R. Amlord (颜世敏), Apr. 3, 2013
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (good-enough? guess prev-guess x)
-  (< (abs (- (improve guess x) guess))(* guess 0.001)))
+(define (good-enough? guess prev-guess)
+  (< (abs (- guess prev-guess))(* guess 0.001)))
 
 (define (sqrt-iter guess prev-guess x)
-  (if (good-enough? guess prev-guess x)
+  (if (good-enough? guess prev-guess)
       guess
       (sqrt-iter (improve guess x) guess x)))
 
@@ -18,7 +19,7 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (sqrt x)
+(define (sqroot x)
   (sqrt-iter 1.0 0 x))
 
-(sqrt 0.00001)
+(sqroot 0.00001)

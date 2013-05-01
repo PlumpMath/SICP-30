b445bcc038ca680f35dac9c1de5adf78fd361629
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-01 21:49:00 +0800

    Add implementation for Example: Searching_for_Divisors.
    Included are the following 2 files:
    
    	Searching_for_Divisors.scm
    	Testing_for_Searching_for_Divisors.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 1st, 2013
    							   Xi'an, China

---
+++ Examples_to_Practice/Chapter_1/Testing_for_Primality/Searching_for_Divisors/Testing_for_Searching_for_Divisors.scm
@@ -0,0 +1,34 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Testing_for_'Searching_for_Divisors.scm'
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 1st, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(prime? 2)
+
+(prime? 3)
+
+(prime? 4)
+
+(prime? 6)
+
+(prime? 63)
+
+(prime? 561)
+
+(prime? 1105)
+
+(prime? 1729)
+
+(prime? 2465)
+
+(prime? 2821)
+
+(prime? 6601)

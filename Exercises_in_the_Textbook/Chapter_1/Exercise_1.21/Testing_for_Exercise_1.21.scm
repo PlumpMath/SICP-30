b85430b5392cf658ab9180fd85ef5b87bfe5b3c6
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-02 00:04:45 +0800

    Add my answer to Exercise_1.21.
    Included are the following 4 files:
    
    	Exercise_1.21.pdf
    	Exercise_1.21.scm
    	Exercise_1.21.tm
    	Testing_for_Exercise_1.21.scm
    
    Thank you!
    							Lawrence R. Amlord
    							  May 2nd, 2013
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_1/Exercise_1.21/Testing_for_Exercise_1.21.scm
@@ -0,0 +1,20 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Testing_for_Exercise_1.21.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 1st, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(smallest-divisor 199)
+;Value: 199
+
+(smallest-divisor 1999)
+;Value: 1999
+
+(smallest-divisor 19999)
+;Value: 7

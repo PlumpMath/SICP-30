93d51a61b043d207a31fd0bafcd8c946ac48cc78
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-01 21:56:40 +0800

    Add implementation for Example: The Fermat Test.
    Included are the following 2 files:
    
    	Testing_for_The_Fermat_Test.scm
    	The_Fermat_Test.scm
    
    Thank you!
    							Lawrence R. Amlord
    							  May 1st, 2013
    							  Xi'an, China

---
+++ Examples_to_Practice/Chapter_1/Testing_for_Primality/The_Fermat_Test/Testing_for_The_Fermat_Test.scm
@@ -0,0 +1,34 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Testing_for_"The_Fermat_Test.scm"
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 1st, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(fast-prime? 2 10)
+
+(fast-prime? 3 10)
+
+(fast-prime? 4 10)
+
+(fast-prime? 6 10)
+
+(fast-prime? 63 100)
+
+(fast-prime? 561 1000)
+
+(fast-prime? 1105 10000)
+
+(fast-prime? 1729 10000)
+
+(fast-prime? 2465 10000)
+
+(fast-prime? 2821 10000)
+
+(fast-prime? 6601 10000)
---
+++ Examples_to_Practice/Chapter_1/Testing_for_Primality/The_Fermat_Test/The_Fermat_Test.scm
@@ -0,0 +1,47 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       The_Fermat_Test.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 1st, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Definition of Procedures
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+	((fermat-test n) (fast-prime? n (- times 1)))
+	(else false)))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (expmod base expt m)
+  (cond ((= expt 0) 1)
+	((even? expt)
+	 (remainder (square (expmod base (/ expt 2) m))
+		    m))
+	(else (remainder (* base (expmod base (- expt 1) m))
+			 m))))
+
+(define square
+  (lambda (x) (* x x)))

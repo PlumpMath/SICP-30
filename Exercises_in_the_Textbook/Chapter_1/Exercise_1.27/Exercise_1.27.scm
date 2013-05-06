e29baa1c3c35f35fa217244b66429c55449c0708
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-06 20:51:13 +0800

    Add my answer to Exercise 1.27.
    Included are the following 4 files:
    
    	Exercise_1.27.pdf
    	Exercise_1.27.scm
    	Exercise_1.27.tm
    	Test_for_Exercise_1.27.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 6th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_1/Exercise_1.27/Exercise_1.27.scm
@@ -0,0 +1,89 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_1.27.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 6th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (carm-fool n)
+  (carm-test n 1))
+
+(define (carm-test n a)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (cond ((>= a n) true)
+	((try-it a) (carm-test n (+ a 1)))
+	(else false)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else
+	 (remainder (* base (expmod base (- exp 1) m))
+		    m))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define square
+  (lambda (x) (* x x)))
+
+;Value: carm-fool
+
+;Value: carm-test
+
+;Value: expmod
+
+;Value: even?
+
+;Value: square
+
+(carm-fool 561)
+;Value: #f
+
+;Value: carm-test
+
+(carm-fool 561)
+;Value: #f
+
+;Value: carm-test
+
+(carm-fool 561)
+;Value: #t
+
+(carm-fool 1105)
+;Value: #t
+
+(carm-fool 1729)
+;Value: #t
+
+(carm-fool 2465)
+;Value: #t
+
+(carm-fool 2821)
+;Value: #t
+
+(carm-fool 6601)
+;Value: #t
+
---
+++ Exercises_in_the_Textbook/Chapter_1/Exercise_1.27/Test_for_Exercise_1.27.scm
@@ -0,0 +1,53 @@
+Copyright (C) 2011 Massachusetts Institute of Technology
+This is free software; see the source for copying conditions. There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+Image saved on Tuesday November 8, 2011 at 10:45:46 PM
+  Release 9.1.1     || Microcode 15.3 || Runtime 15.7 || SF 4.41
+  LIAR/x86-64 4.118 || Edwin 3.116
+;You are in an interaction window of the Edwin editor.
+;Type `C-h' for help, or `C-h t' for a tutorial.
+;`C-h m' will describe some commands.
+;`C-h' means: hold down the Ctrl key and type `h'.
+;Package: (user)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_1.27.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 6th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: carm-fool
+
+;Value: carm-test
+
+;Value: expmod
+
+;Value: even?
+
+;Value: square
+
+(carm-fool 561)
+;Value: #t
+
+(carm-fool 1105)
+;Value: #t
+
+(carm-fool 1729)
+;Value: #t
+
+(carm-fool 2465)
+;Value: #t
+
+(carm-fool 2821)
+;Value: #t
+
+(carm-fool 6601)
+;Value: #t

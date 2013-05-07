5dd73ecfa6f2f99e53787ea8a0e8fa743ea83787
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-07 15:48:07 +0800

    Add Example: Exponentiation-Linear_Recursive_Version in Chapter_1.
    Included are the following 2 files:
    
    	Exponentiation-Linear_Recursive_Version.scm
    	Test_for_Exponentiation-Linear_Recursive_Version.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 7th, 2013
    							   Xi'an, China

---
+++ Examples_to_Practice/Chapter_1/Exponentiation/Exponentiation-Linear_Recursive_Version/Test_for_Exponentiation-Linear_Recursive_Version.scm
@@ -0,0 +1,37 @@
+Copyright (C) 2011 Massachusetts Institute of Technology
+This is free software; see the source for copying conditions. There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+Image saved on Tuesday November 8, 2011 at 10:45:46 PM
+  Release 9.1.1     || Microcode 15.3 || Runtime 15.7 || SF 4.41
+  LIAR/x86-64 4.118 || Edwin 3.116
+;You are in an interaction window of the Edwin editor.
+;Type `C-h' for help, or `C-h t' for a tutorial.
+;`C-h m' will describe some commands.
+;`C-h' means: hold down the Ctrl key and type `h'.
+;Package: (user)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exponentiation-Linear_Recursive_Version.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 7th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: expt
+
+(expt 2 0)
+;Value: 1
+
+(expt 2 1)
+;Value: 2
+
+(expt 2 10)
+;Value: 1024
+

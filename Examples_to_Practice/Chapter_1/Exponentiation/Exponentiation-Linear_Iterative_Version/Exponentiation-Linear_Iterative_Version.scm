ee15dbccf6d2ae9420399369aef2337e1aa5d162
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-07 17:51:42 +0800

    Add Example: Exponentiation-Linear_Iterative_Version in Chapter_1.
    Included are the following 2 files:
    
    	Exponentiation-Linear_Iterative_Version.scm
    	Test_for_Exponentiation-Linear_Iterative_Version.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 7th, 2013
    							   Xi'an, China

---
+++ Examples_to_Practice/Chapter_1/Exponentiation/Exponentiation-Linear_Iterative_Version/Exponentiation-Linear_Iterative_Version.scm
@@ -0,0 +1,35 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exponentiation-Linear_Iterative_Version.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 7th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (expt b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b
+		 (- counter 1)
+		 (* b product))))

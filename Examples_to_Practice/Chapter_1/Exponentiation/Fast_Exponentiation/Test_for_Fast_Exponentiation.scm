8115e463f34526776717e0be97fbb2b4f91915ae
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-07 17:53:55 +0800

    Add Example: Fast_Exponentiation in Chapter_1.
    Included are the following 2 files:
    
    	Fast_Exponentiation.scm
    	Test_for_Fast_Exponentiation.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 7th, 2013
    							   Xi'an, China

---
+++ Examples_to_Practice/Chapter_1/Exponentiation/Fast_Exponentiation/Test_for_Fast_Exponentiation.scm
@@ -0,0 +1,40 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Fast_Exponentiation.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 7th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: fast-expt
+
+;Value: even?
+
+;Value: square
+
+(fast-expt 2 0)
+;Value: 1
+
+(fast-expt 2 1)
+;Value: 2
+
+(fast-expt 2 10)
+;Value: 1024

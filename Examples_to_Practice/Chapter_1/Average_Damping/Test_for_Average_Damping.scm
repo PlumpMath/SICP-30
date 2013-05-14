f095b515f2f213fe6fadc0fd65c607647e09441f
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-15 00:25:53 +0800

    Update Test_for_Average_Damping.scm

--- Examples_to_Practice/Chapter_1/Average_Damping/Test_for_Average_Damping.scm
+++ Examples_to_Practice/Chapter_1/Average_Damping/Test_for_Average_Damping.scm
@@ -29,12 +29,12 @@
 ;Value: sqrt
 
 (define (fixed-point f first-guess)
-  (define (good-enough? v1 v2)
+  (define (close-enough? v1 v2)
     (let ((tolerance 0.00001))
       (< (abs (- v1 v2)) tolerance)))
   (define (try guess)
     (let ((next (f guess)))
-      (if (good-enough? guess next)
+      (if (close-enough? guess next)
 	  next
 	  (try next))))
   (try first-guess))

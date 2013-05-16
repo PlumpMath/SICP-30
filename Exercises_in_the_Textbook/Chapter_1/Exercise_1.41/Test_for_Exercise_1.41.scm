f4387f101e04cff1dbaa3f71509a3cb19c788c40
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-16 13:15:19 +0800

    Add my answer to Exercise 1.41.
    	Exercise_1.41.pdf
    	Exercise_1.41.scm
    	Exercise_1.41.tm
    	Test_for_Exercise_1.41.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 16th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_1/Exercise_1.41/Test_for_Exercise_1.41.scm
@@ -0,0 +1,24 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_1.41.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 16th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (inc x)
+  (+ x 1))
+
+(((double (double double)) inc) 5)
+
+;Value: double
+
+;Value: inc
+
+;Value: 21
+

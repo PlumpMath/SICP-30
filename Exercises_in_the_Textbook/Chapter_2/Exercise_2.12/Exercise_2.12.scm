88fe6fc42ca8445ccb23e0cc7174d99bf238d04b
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-26 15:51:09 +0800

    Add my answer to Exercise 2.12.
    Included are the following 2 files:
    
    	Exercise_2.12.scm
    	Test_for_Exercise_2.12.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 26th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.12/Exercise_2.12.scm
@@ -0,0 +1,40 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_2.12.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 26th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-center-percent c p)
+  (make-interval (* c (- 1.0 p))
+		 (* c (+ 1.0 p))))
+
+(define (percent i)
+  (* (/ (/ (- (upper-bound i) (lower-bound i))
+	   2)
+	(/ (+ (lower-bound i) (upper-bound i))
+	   2))
+     100))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i))
+     2))
---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.12/Test_for_Exercise_2.12.scm
@@ -0,0 +1,55 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_2.12.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 26th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-interval a b)
+  (cons a b))
+
+(define (lower-bound i)
+  (car i))
+
+(define (upper-bound i)
+  (cdr i))
+
+(define (print-center-percent i)
+  (display (center i))
+  (display " with ")
+  (display (percent i))
+  (display "% ")
+  (display "tolerance."))
+
+;Value: make-center-percent
+
+;Value: percent
+
+;Value: center
+
+;Value: make-interval
+
+;Value: lower-bound
+
+;Value: upper-bound
+
+;Value: print-center-percent
+
+(print-center-percent (make-center-percent 1.0 0.06))
+1. with 6.000000000000005% tolerance
+;Unspecified return value
+
+(print-center-percent (make-center-percent 8 0.005))
+8. with .4999999999999949% tolerance
+;Unspecified return value
+
+(print-center-percent (make-center-percent 1024.0 0.0125))
+1024. with 1.2499999999999956% tolerance
+;Unspecified return value
+

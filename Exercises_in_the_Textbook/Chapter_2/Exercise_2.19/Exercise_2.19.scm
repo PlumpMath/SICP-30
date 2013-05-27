561abcfce001b087235c33e35da8c0250aefa22e
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-28 02:14:28 +0800

    Add my answer to Exercise 2.19.
    Included are the following 4 files:
    
    	Exercise_2.19.pdf
    	Exercise_2.19.scm
    	Exercise_2.19.tm
    	Test_for_Exercise_2.19.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 29th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.19/Exercise_2.19.scm
@@ -0,0 +1,45 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_2.19.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 28th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (no-more? coin-values)) 0)
+	(else
+	 (+ (cc amount
+		(except-first-denomination coin-values))
+	    (cc (- amount
+		   (first-denomination coin-values))
+		coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.19/Test_for_Exercise_2.19.scm
@@ -0,0 +1,52 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_2.19.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 28th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (reverse items)
+  (define (last-element items)
+    (if (null? (cdr items))
+	(car items)
+	(last-element (cdr items))))
+  (define (former-sublist items)
+    (if (null? (cdr items))
+	'()
+	(cons (car items) (former-sublist (cdr items)))))
+  (if (null? (cdr items))
+      items
+      (cons (last-element items) (reverse (former-sublist items)))))
+;Value: reverse
+
+;Value: cc
+
+;Value: first-denomination
+
+;Value: except-first-denomination
+
+;Value: no-more?
+
+(define us-coins (list 50 25 10 5 1))
+;Value: us-coins
+
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+;Value: uk-coins
+
+(cc 100 us-coins)
+;Value: 292
+
+(cc 100 (reverse us-coins))
+;Value: 292
+
+(cc 100 uk-coins)
+;Value: 104561
+
+(cc 100 (reverse uk-coins))
+;Value: 104561

067e019842e87cbe8dd10dc2339895377d565787
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-27 17:05:26 +0800

    Add my answer to Exercise 2.17.
    Included are the following 4 files:
    
    	Exercise_2.17.pdf
    	Exercise_2.17.scm
    	Exercise_2.17.tm
    	Test_for_Exercise_2.17.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 27th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.17/Test_for_Exercise_2.17.scm
@@ -0,0 +1,36 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_2.17.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 27th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: last-pair
+
+(last-pair (list 23 72 149 34))
+;Value 21: (34)
+
+(define one-through-four (list 1 2 3 4))
+;Value: one-through-four
+
+(last-pair one-through-four)
+;Value 23: (4)
+
+(define squares (list 1 4 9 16 25))
+;Value: squares
+
+(last-pair squares)
+;Value 24: (25)
+
+
+(define odds (list 1 3 5 7))
+;Value: odds
+
+(cdr odds)
+;Value 22: (3 5 7)

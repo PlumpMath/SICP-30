646c11f3078ab11b09478ed0d32435ba4e046f07
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-30 01:28:01 +0800

    Add my answer to Exercise 2.25.
    Included are the following 3 files:
    
    	Exercise_2.25.pdf
    	Exercise_2.25.scm
    	Exercise_2.25.tm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 30th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.25/Exercise_2.25.scm
@@ -0,0 +1,50 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_2.25.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 29th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9))))))
+;Value: 7
+
+(car (car (list (list 7))))
+;Value: 7
+
+(car
+ (cdr
+  (car
+   (cdr
+    (car
+     (cdr
+      (car
+       (cdr
+	(car
+	 (cdr
+	  (car
+	   (cdr (list 1
+		      (list 2
+			    (list 3
+				  (list 4
+					(list 5
+					      (list 6 7))))))))))))))))))
+;Value: 7

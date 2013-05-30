5dfcd2384f0e0644e7bde83635936dc08bf803ce
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-30 19:31:32 +0800

    Add my answer to Exercise 2.28.
    Included are the following 4 files:
    
    	Exercise_2.28.pdf
    	Exercise_2.28.scm
    	Exercise_2.28.tm
    	Test_for_Exercise_2.28.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 30th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.28/Test_for_Exercise_2.28.scm
@@ -0,0 +1,21 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_2.28.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 30th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: fringe
+
+(define x (list (list 1 2) (list 3 4)))
+;Value: x
+
+(fringe x)
+;Value 21: (1 2 3 4)
+

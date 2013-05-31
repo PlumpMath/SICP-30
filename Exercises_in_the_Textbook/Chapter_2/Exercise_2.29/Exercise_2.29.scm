db0ee24b554ca2ea093f374d6953b64ca36dcafa
Lawrence R. Amlord <informlarry@gmail.com>
2013-05-31 17:47:20 +0800

    Add my answer to Exercise 2.29.
    Included are the following 4 files:
    
    	Exercise_2.29.pdf
    	Exercise_2.29.scm
    	Exercise_2.29.tm
    	Test_for_Exercise_2.29.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   May 31st, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.29/Exercise_2.29.scm
@@ -0,0 +1,117 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_2.29.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 30th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;
+;; Subproblem a.
+;;;;;;;;;;;;;;;;;;;;
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (left-branch m)
+  (car m))
+
+(define (right-branch m)
+  (car (cdr m)))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (branch-length b)
+  (car b))
+
+(define (branch-structure b)
+  (car (cdr b)))
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; Subproblem b.
+;;;;;;;;;;;;;;;;;;;;
+
+(define (total-weight m)
+  (cond ((null? m) 0)
+	  ((not (pair? m)) m)
+	  (else
+	   (+ (total-weight (branch-structure (left-branch m)))
+	      (total-weight (branch-structure (right-branch m)))))))
+
+
+;; An alternative implementation
+
+(define (total-weight m)
+  (cond ((null? m) 0)
+	((not (pair? m)) m)
+	(else
+	 (+ (branch-weight (left-branch m))
+	    (branch-weight (right-branch m))))))
+
+(define (branch-weight b)
+  (if (not (pair? (branch-structure b)))
+      (branch-structure b)
+      (total-weight (branch-structure b))))
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; Subproblem c.
+;;;;;;;;;;;;;;;;;;;;
+
+(define (balanced? m)
+  (and (= (torque (left-branch m))
+	  (torque (right-branch m)))
+       (branch-balanced? (left-branch m))
+       (branch-balanced? (right-branch m))))
+
+(define (torque b)
+  (* (branch-length b)
+     (branch-weight b)))
+
+(define (branch-weight b)
+  (if (not (pair? (branch-structure b)))
+      (branch-structure b)
+      (+ (branch-weight (left-branch (branch-structure b)))
+	 (branch-weight (right-branch (branch-structure b))))))
+
+(define (branch-balanced? b)
+  (if (not (pair? (branch-structure b)))
+      #t
+      (balanced? (branch-structure b))))
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; Subproblem d.
+;;;;;;;;;;;;;;;;;;;;
+
+(define (make-mobile left right)
+  (cons left right))
+
+(define (make-branch left right)
+  (cons left right))
+
+(define (right-branch m)
+  (cdr m))
+
+(define (branch-structure b)
+  (cdr b))
---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.29/Test_for_Exercise_2.29.scm
@@ -0,0 +1,251 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_2.29.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       May 30th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: make-mobile
+
+;Value: left-branch
+
+;Value: right-branch
+
+;Value: make-branch
+
+;Value: branch-length
+
+;Value: branch-structure
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Test for subproblem b.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Build the first mobile
+
+(define left11 (make-branch 5 8))
+;Value: left11
+
+(define right11 (make-branch 7 10))
+;Value: right11
+
+(define m11 (make-mobile left11 right11))
+;Value: m11
+
+(define left12 (make-branch 4 6))
+;Value: left12
+
+(define right12 (make-branch 1 3))
+;Value: right12
+
+(define m12 (make-mobile left12 right12))
+;Value: m12
+
+(define left10 (make-branch 12 m11))
+;Value: left10
+
+(define right10 (make-branch 5 m12))
+;Value: right10
+
+(define m10 (make-mobile left10 right10))
+;Value: m10
+
+;; Build the second mobile
+
+(define left21 (make-branch 2 25))
+;Value: left21
+
+(define right21 (make-branch 5 10))
+;Value: right21
+
+(define m21 (make-mobile left21 right21))
+;Value: m21
+
+(define left20 (make-branch 4 m21))
+;Value: left20
+
+(define left22 (make-branch 4 30))
+;Value: left22
+
+(define right22 (make-branch 3 40))
+;Value: right22
+
+(define m22 (make-mobile left22 right22))
+;Value: m22
+
+(define right20 (make-branch 2 m22))
+;Value: right20
+
+(define m20 (make-mobile left20 right20))
+;Value: m20
+
+
+;; Test for the first implementation of total-weight
+
+;Value: total-weight
+
+(total-weight m10)
+;Value: 27
+
+(total-weight m20)
+;Value: 105
+
+
+;; Test for the second implementation of total-weight
+
+;Value: total-weight
+
+;Value: branch-weight
+
+(total-weight m10)
+;Value: 27
+
+(total-weight m20)
+;Value: 105
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Test for subproblem c.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: balanced?
+
+;Value: torque
+
+;Value: branch-weight
+
+;Value: branch-balanced?
+
+(balanced? m10)
+;Value: #f
+
+(balanced? m20)
+;Value: #t
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Test for subproblem d.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: make-mobile
+
+;Value: make-branch
+
+;Value: right-branch
+
+;Value: branch-structure
+
+
+;; Build the first mobile
+
+(define left11 (make-branch 5 8))
+;Value: left11
+
+(define right11 (make-branch 7 10))
+;Value: right11
+
+(define m11 (make-mobile left11 right11))
+;Value: m11
+
+(define left12 (make-branch 4 6))
+;Value: left12
+
+(define right12 (make-branch 1 3))
+;Value: right12
+
+(define m12 (make-mobile left12 right12))
+;Value: m12
+
+(define left10 (make-branch 12 m11))
+;Value: left10
+
+(define right10 (make-branch 5 m12))
+;Value: right10
+
+(define m10 (make-mobile left10 right10))
+;Value: m10
+
+
+;; Build the second mobile
+
+(define left21 (make-branch 2 25))
+;Value: left21
+
+(define right21 (make-branch 5 10))
+;Value: right21
+
+(define m21 (make-mobile left21 right21))
+;Value: m21
+
+(define left20 (make-branch 4 m21))
+;Value: left20
+
+(define left22 (make-branch 4 30))
+;Value: left22
+
+(define right22 (make-branch 3 40))
+;Value: right22
+
+(define m22 (make-mobile left22 right22))
+;Value: m22
+
+(define right20 (make-branch 2 m22))
+;Value: right20
+
+(define m20 (make-mobile left20 right20))
+;Value: m20
+
+
+;; Test for the first implementation of total-weight
+
+;Value: total-weight
+
+(total-weight m10)
+;Value: 27
+
+(total-weight m20)
+;Value: 105
+
+
+;; Test for the second implementation of total-weight
+
+;Value: total-weight
+
+;Value: branch-weight
+
+(total-weight m10)
+;Value: 27
+
+(total-weight m20)
+;Value: 105
+
+
+;; Test whether the two mobiles are balanced
+
+;Value: balanced?
+
+;Value: torque
+
+;Value: branch-weight
+
+;Value: branch-balanced?
+
+(balanced? m10)
+;Value: #f
+
+(balanced? m20)
+;Value: #t
+
+
+
+

4d2c253a7a1bae92e12c95e1b84fe56e2ef62b0b
Lawrence R. Amlord <informlarry@gmail.com>
2013-06-01 21:08:36 +0800

    Add my answer to Exercise 2.34.
    Included are the following 4 files:
    
    	Exercise_2.34.pdf
    	Exercise_2.34.scm
    	Exercise_2.34.tm
    	Test_for_Exercise_2.34.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   Jun 1st, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.34/Test_for_Exercise_2.34.scm
@@ -0,0 +1,25 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_2.34.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       Jun 1st, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Value: horner-eval
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+;Value: accumulate
+
+(horner-eval 2 (list 1 3 0 5 0 1))
+;Value: 79
+

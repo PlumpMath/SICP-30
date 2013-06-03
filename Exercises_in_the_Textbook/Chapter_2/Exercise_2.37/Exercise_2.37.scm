c44389fb64e63f2e69064303cb16be57ea55d577
Lawrence R. Amlord <informlarry@gmail.com>
2013-06-04 01:46:56 +0800

    Add my answer to Exercise 2.37.
    Included are the following 4 files:
    
    	Exercise_2.37.pdf
    	Exercise_2.37.scm
    	Exercise_2.37.tm
    	Test_for_Exercise_2.37.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   Jun 4th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.37/Exercise_2.37.scm
@@ -0,0 +1,44 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_2.37.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       Jun 3rd, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (i)
+	 (dot-product v i))
+       m))
+
+(define (transpose mat)
+  (accumulate-n (lambda (x y) (cons x y))
+		'()
+		mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (v)
+	   (matrix-*-vector cols v))
+	 m)))
---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.37/Test_for_Exercise_2.37.scm
@@ -0,0 +1,60 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Exercise_2.37.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       Jun 3rd, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+;Value: accumulate
+
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+	    (accumulate-n op init (map cdr seqs)))))
+;Value: accumulate-n
+
+;Value: dot-product
+
+;Value: matrix-*-vector
+
+;Value: transpose
+
+;Value: matrix-*-matrix
+
+(define v (list 1 2 4 8))
+;Value: v
+
+(define w (list 3 5 7 9))
+;Value: w
+
+(define m (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
+;Value: m
+
+(define n (list (list 2 3 4) (list 5 6 7) (list 8 7 6) (list 5 4 3)))
+;Value: n
+
+(dot-product v w)
+;Value: 113
+
+(matrix-*-vector m v)
+;Value 20: (49 86 124)
+
+(transpose m)
+;Value 24: ((1 4 6) (2 5 7) (3 6 8) (4 6 9))
+
+(matrix-*-matrix m n)
+;Value 25: ((56 52 48) (111 108 105) (156 152 148))
+

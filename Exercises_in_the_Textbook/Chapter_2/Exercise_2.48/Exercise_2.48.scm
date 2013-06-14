6a70297afe5d655dcdab29cab111f4e678c56023
Lawrence R. Amlord <informlarry@gmail.com>
2013-06-14 17:33:39 +0800

    Add my answer to Exercise 2.48.
    Included is the following single file:
    
    	Exercise_2.48.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   Jun 14th, 2013
    							   Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.48/Exercise_2.48.scm
@@ -0,0 +1,34 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_2.48.scm
+;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       informlarry@gmail.com
+;;                       Jun 14th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-segment start-point end-point)
+  (cons start-point end-point))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))

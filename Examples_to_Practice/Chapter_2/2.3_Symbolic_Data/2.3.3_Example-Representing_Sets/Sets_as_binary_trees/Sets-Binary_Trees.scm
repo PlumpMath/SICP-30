db64206c27b7496ad42c19405f4b4779c1006d6f
Lawrence R. Amlord <informlarry@gmail.com>
2013-07-20 20:56:27 +0800

    Add Example: Sets as binary tree.
    Included is the following file:
    
    	Sets-Binary_Trees.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   Jul 20th, 2013
    							   Xi'an, China

---
+++ Examples_to_Practice/Chapter_2/2.3_Symbolic_Data/2.3.3_Example-Representing_Sets/Sets_as_binary_trees/Sets-Binary_Trees.scm
@@ -0,0 +1,60 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Sets-Binary_Trees.scm
+;;                       originated from SICP
+;;                       edited by Lawrence R. Amlord(颜世敏)
+;;                       informlarry@gmail.com
+;;                       Jul 20th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 1984-2013 Harold Abelson and Gerald Jay Sussman
+;; with Julie Sussman
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Represent trees by using lists 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((= x (entry set)) true)
+	((< x (entry set))
+	 (element-of-set? x (left-branch set)))
+	(else    ; x is greater than the entry
+	 (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+	((= x (entry set)) set)
+	((< x (entry set))
+	 (make-tree (entry set)
+		    (adjoin-set x (left-branch set))
+		    (right-branch set)))
+	(else
+	 (make-tree (entry set)
+		    (left-branch set)
+		    (adjoin-set x (right-branch set))))))

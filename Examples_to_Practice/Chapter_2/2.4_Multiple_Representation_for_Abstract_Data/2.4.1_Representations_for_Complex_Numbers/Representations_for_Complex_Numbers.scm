ae7a3b357a3590096c0b2adf280546252836afac
Lawrence R. Amlord <informlarry@gmail.com>
2013-07-26 20:59:16 +0800

    Add Code of 2.4.1.
    Included is the following file:
    
    	Representations_for_Complex_Numbers.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   Jul 26th, 2013
    							   Xi'an, China

---
+++ Examples_to_Practice/Chapter_2/2.4_Multiple_Representation_for_Abstract_Data/2.4.1_Representations_for_Complex_Numbers/Representations_for_Complex_Numbers.scm
@@ -0,0 +1,97 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Representations_for_Complex_Numbers.scm
+;;                       originated from SICP
+;;                       edited by Lawrence R. Amlord(颜世敏)
+;;                       informlarry@gmail.com
+;;                       Jul 26th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 1984-2013 Harold Abelson and Gerald Jay Sussman
+;; with Julie Sussman
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;  Arithmetic on Complex Numbers 
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+		       (+ (imag-part z1) (imag-part z2))))
+
+(define (sub-complex z1 z2)
+  (make-from-real-imag (- (real-part z1) (real-part z2))
+		       (- (imag-part z1) (imag-part z2))))
+
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+		     (+ (angle z1) (angle z2))))
+
+(define (div-complex z1 z2)
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+		     (- (angle z1) (angle z2))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Ben's Representation
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (real-part z) (car z))
+
+(define (imag-part z) (cdr z))
+
+(define (magnitude z)
+  (sqrt (+ (square (real-part z))
+	   (square (imag-part z)))))
+
+(define (angle z)
+  (atan (imag-part z) (real-part z)))
+
+(define (make-from-real-imag x y) (cons x y))
+
+(define (make-from-mag-ang r a)
+  (cons (* r (cos a))
+	(* r (sin a))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Alyssa's Representation
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (real-part z)
+  (* (magnitude z) (cos (angle z))))
+
+(define (imag-part z)
+  (* (magnitude z) (sin (angle z))))
+
+(define (magnitude z) (car z))
+
+(define (angle z) (cdr z))
+
+(define (make-from-real-imag x y)
+  (cons (sqrt (+ (square x)
+		 (square y)))
+	(atan y x)))
+
+(define (make-from-mag-ang r a) (cons r a))
+

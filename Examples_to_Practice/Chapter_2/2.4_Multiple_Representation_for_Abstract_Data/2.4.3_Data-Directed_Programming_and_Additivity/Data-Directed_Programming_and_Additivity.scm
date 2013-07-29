1281eac17b4a7f9d64a8dd157da94280998c5dcd
Lawrence R. Amlord <informlarry@gmail.com>
2013-07-29 17:05:45 +0800

    Update code of section 2.4.3.
    Modified is the following file:
    
    	Data-Directed_Programming_and_Additivity.scm
    
    Thank you!
    							Lawrence R. Amlord
    							   Jul 29th, 2013
    							   Xi'an, China

--- Examples_to_Practice/Chapter_2/2.4_Multiple_Representation_for_Abstract_Data/2.4.3_Data-Directed_Programming_and_Additivity/Data-Directed_Programming_and_Additivity.scm
+++ Examples_to_Practice/Chapter_2/2.4_Multiple_Representation_for_Abstract_Data/2.4.3_Data-Directed_Programming_and_Additivity/Data-Directed_Programming_and_Additivity.scm
@@ -136,3 +136,24 @@
 
 (define (make-from-mag-ang r a)
   ((get 'make-from-mag-ang 'polar) r a))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MAKE-FROM-REAL-IMAG Implemented in Message-Passing Style
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+	  ((eq? op 'imag-part) y)
+	  ((eq? op 'magnitude)
+	   (sqrt (+ (square x) (square y))))
+	  ((eq? op 'angle) (atan y x))
+	  (else
+	   (error "Uknown op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+(define (apply-generic op arg) (arg op))

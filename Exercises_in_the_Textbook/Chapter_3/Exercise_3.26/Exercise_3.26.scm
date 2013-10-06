d4f8fb9fccd388813ba6157ef766325792549907
Lawrence R. Amlord <informlarry@gmail.com>
2013-10-06 13:45:26 +0800

    Update my answer to Exercise 3.26.
    This time, I've extend the program to handle an arbitrary dimentional
    table. Modified is the following file:
    
    	Exercise_3.26.scm
    
    Thank you!
    							Lawrence X. Amlord
    							  Oct 6th, 2013
    							  Xi'an, China

--- Exercises_in_the_Textbook/Chapter_3/Exercise_3.26/Exercise_3.26.scm
+++ Exercises_in_the_Textbook/Chapter_3/Exercise_3.26/Exercise_3.26.scm
@@ -451,15 +451,15 @@
 ;Value: lookup
 
 (define (insert! given-key-1 given-key-2 given-value table)
-  (let ((record (make-record given-key-1 given-key-2 given-value)))
+  (let ((2D-record (make-record given-key-1 given-key-2 given-value)))
     (set-cdr! table
-	      (adjoin-tree-2D record (cdr table))))
+	      (adjoin-tree-2D 2D-record (cdr table))))
   'ok)
 ;Value: insert!
 
 (define (adjoin-tree-2D 2D-record 2D-tree)
   (let ((first-given-key (key 2D-record))
-	(1D-record (cdr 2D-record)))
+	(1D-record (value 2D-record)))
     (if (null? 2D-tree)
 	(make-tree (cons first-given-key
 			 (make-tree 1D-record '() '()))
@@ -468,7 +468,7 @@
 	(let ((first-entry-key (key (entry 2D-tree))))
 	  (cond ((= first-given-key first-entry-key)
 		 (make-tree (cons first-entry-key
-				  (adjoin-tree 1D-record (cdr (entry 2D-tree))))
+				  (adjoin-tree-1D 1D-record (cdr (entry 2D-tree))))
 			    (left-branch 2D-tree)
 			    (right-branch 2D-tree)))
 		((< first-given-key first-entry-key)
@@ -481,7 +481,7 @@
 			    (adjoin-tree-2D 2D-record (right-branch 2D-tree)))))))))
 ;Value: adjoin-tree-2d
 
-(define (adjoin-tree record tree)
+(define (adjoin-tree-1D record tree)
   (if (null? tree)
       (make-tree record '() '())
       (let ((given-key (key record))
@@ -492,13 +492,13 @@
 			  (right-branch tree)))
 	      ((< given-key entry-key)
 	       (make-tree (entry tree)
-			  (adjoin-tree record (left-branch tree))
+			  (adjoin-tree-1D record (left-branch tree))
 			  (right-branch tree)))
 	      (else
 	       (make-tree (entry tree)
 			  (left-branch tree)
-			  (adjoin-tree record (right-branch tree))))))))
-;Value: adjoin-tree
+			  (adjoin-tree-1D record (right-branch tree))))))))
+;Value: adjoin-tree-1D
 
 (define (make-table)
   (list '*table*))
@@ -804,7 +804,7 @@
       'ok)
     (define (adjoin-tree-2D 2D-record 2D-tree)
       (let ((first-given-key (key 2D-record))
-	    (1D-record (cdr 2D-record)))
+	    (1D-record (value 2D-record)))
 	(if (null? 2D-tree)
 	    (make-tree (cons first-given-key
 			     (make-tree 1D-record '() '()))
@@ -813,7 +813,7 @@
 	    (let ((first-entry-key (key (entry 2D-tree))))
 	      (cond ((= first-given-key first-entry-key)
 		     (make-tree (cons first-entry-key
-				      (adjoin-tree 1D-record (cdr (entry 2D-tree))))
+				      (adjoin-tree-1D 1D-record (cdr (entry 2D-tree))))
 				(left-branch 2D-tree)
 				(right-branch 2D-tree)))
 		    ((< first-given-key first-entry-key)
@@ -824,7 +824,7 @@
 		     (make-tree (entry 2D-tree)
 				(left-branch 2D-tree)
 				(adjoin-tree-2D 2D-record (right-branch 2D-tree)))))))))
-    (define (adjoin-tree record tree)
+    (define (adjoin-tree-1D record tree)
       (if (null? tree)
 	  (make-tree record '() '())
 	  (let ((given-key (key record))
@@ -835,12 +835,12 @@
 			      (right-branch tree)))
 		  ((< given-key entry-key)
 		   (make-tree (entry tree)
-			      (adjoin-tree record (left-branch tree))
+			      (adjoin-tree-1D record (left-branch tree))
 			      (right-branch tree)))
 		  (else
 		   (make-tree (entry tree)
 			      (left-branch tree)
-			      (adjoin-tree record (right-branch tree))))))))
+			      (adjoin-tree-1D record (right-branch tree))))))))
 
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
@@ -1104,4 +1104,576 @@
 
 (get-5 HK 086)
 ;Value: chan
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;   Multi-dimentional table
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-tree entry left right)
+  (list entry left right))
+;Value: make-tree
+
+(define (entry tree) (car tree))
+;Value: entry
+
+(define (left-branch tree) (cadr tree))
+;Value: left-branch
+
+(define (right-branch tree) (caddr tree))
+;Value: right-branch
+
+(define (make-record keys value)
+  (if (null? (cdr keys))
+      (cons (car keys) value)
+      (cons (car keys)
+	    (make-record (cdr keys) value))))
+;Value: make-record
+
+(define (key record) (car record))
+;Value: key
+
+(define (value record) (cdr record))
+;Value: value
+
+(define (assoc given-key tree)
+  (if (null? tree)
+      false
+      (let ((entry-key (key (entry tree))))
+	(cond ((= given-key entry-key)
+	       (entry tree))
+	      ((< given-key entry-key)
+	       (assoc given-key (left-branch tree)))
+	      (else
+	       (assoc given-key (right-branch tree)))))))
+;Value: assoc
+
+(define (lookup keys table)
+  (if (null? (cdr keys))
+      (let ((record (assoc (car keys) (cdr table))))
+	(if record
+	    (cdr record)
+	    false))
+      (let ((subtable (assoc (car keys) (cdr table))))
+	(if subtable
+	    (lookup (cdr keys) subtable)
+	    false))))
+;Value: lookup
+
+(define (insert! keys given-value table)
+  (let ((MD-record (make-record keys given-value)))
+    (set-cdr! table
+	      (adjoin-tree-MD MD-record (cdr table))))
+  'ok)
+;Value: insert!
+
+(define (adjoin-tree-MD MD-record MD-tree)
+  (if (not (pair? (value MD-record)))
+      (adjoin-tree-1D MD-record MD-tree)
+      (let ((first-given-key (key MD-record))
+	    (subrecord (value MD-record)))
+	(if (null? MD-tree)
+	    (make-tree (cons first-given-key
+			     (adjoin-tree-MD subrecord '()))
+		       '()
+		       '())
+	    (let ((first-entry-key (key (entry MD-tree))))
+	      (cond ((= first-given-key first-entry-key)
+		     (make-tree (cons first-entry-key
+				      (adjoin-tree-MD subrecord (cdr (entry MD-tree))))
+				(left-branch MD-tree)
+				(right-branch MD-tree)))
+		    ((< first-given-key first-entry-key)
+		     (make-tree (entry MD-tree)
+				(adjoin-tree-MD MD-record (left-branch MD-tree))
+				(right-branch MD-tree)))
+		    (else
+		     (make-tree (entry MD-tree)
+				(left-branch MD-tree)
+				(adjoin-tree-MD MD-record (right-branch MD-tree))))))))))
+;Value: adjoin-tree-md
+
+(define (adjoin-tree-1D record tree)
+  (if (null? tree)
+      (make-tree record '() '())
+      (let ((given-key (key record))
+	    (entry-key (key (entry tree))))
+	(cond ((= given-key entry-key)
+	       (make-tree record
+			  (left-branch tree)
+			  (right-branch tree)))
+	      ((< given-key entry-key)
+	       (make-tree (entry tree)
+			  (adjoin-tree-1D record (left-branch tree))
+			  (right-branch tree)))
+	      (else
+	       (make-tree (entry tree)
+			  (left-branch tree)
+			  (adjoin-tree-1D record (right-branch tree))))))))
+;Value: adjoin-tree-1d
+
+(define (make-table)
+  (list '*table*))
+;Value: make-table
+
+;; Universities and Colleges
+
+(define Havard 001)
+;Value: havard
+
+(define MIT 002)
+;Value: mit
+
+(define Cambridge 003)
+;Value: cambridge
+
+(define Oxford 004)
+;Value: oxford
+
+(define Harvy-Mudd 005)
+;Value: harvy-mudd
+
+(define Stanford 006)
+;Value: stanford
+
+(define HKU 007)
+;Value: hku
+
+(define TokyoU 008)
+;Value: tokyou
+
+
+;; Schools and Departments
+
+(define Philosophy 01)
+;Value: philosophy
+
+(define Mathematics 02)
+;Value: mathematics
+
+(define Law 03)
+;Value: law
+
+(define Physics 04)
+;Value: physics
+
+(define Chemistry 05)
+;Value: chemistry
+
+(define EECS 06)
+;Value: eecs
+
+(define Biology 07)
+;Value: biology
+
+(define Mechanics 08)
+;Value: mechanics
+
+(define Bussiness 09)
+;Value: bussiness
+
+;; Students Rank
+
+(define Undergraduate 01)
+;Value: undergraduate
+
+(define Graduate 02)
+;Value: graduate
+
+
+
+(define T8 (make-table))
+;Value: t8
+
+(lookup (list Stanford Mathematics Undergraduate 120) T8)
+;Value: #f
+
+(insert! (list Stanford Mathematics Undergraduate 120) 'John T8)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;   Multi-dimentional table
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-tree entry left right)
+  (list entry left right))
+;Value: make-tree
+
+(define (entry tree) (car tree))
+;Value: entry
+
+(define (left-branch tree) (cadr tree))
+;Value: left-branch
+
+(define (right-branch tree) (caddr tree))
+;Value: right-branch
+
+(define (make-record keys value)
+  (if (null? (cdr keys))
+      (cons (car keys) value)
+      (cons (car keys)
+	    (make-record (cdr keys) value))))
+;Value: make-record
+
+(define (key record) (car record))
+;Value: key
+
+(define (value record) (cdr record))
+;Value: value
+
+(define (assoc given-key tree)
+  (if (null? tree)
+      false
+      (let ((entry-key (key (entry tree))))
+	(cond ((= given-key entry-key)
+	       (entry tree))
+	      ((< given-key entry-key)
+	       (assoc given-key (left-branch tree)))
+	      (else
+	       (assoc given-key (right-branch tree)))))))
+;Value: assoc
+
+(define (lookup keys table)
+  (if (null? (cdr keys))
+      (let ((record (assoc (car keys) (cdr table))))
+	(if record
+	    (cdr record)
+	    false))
+      (let ((subtable (assoc (car keys) (cdr table))))
+	(if subtable
+	    (lookup (cdr keys) subtable)
+	    false))))
+;Value: lookup
+
+(define (insert! keys given-value table)
+  (let ((MD-record (make-record keys given-value)))
+    (set-cdr! table
+	      (adjoin-tree-MD MD-record (cdr table))))
+  'ok)
+;Value: insert!
+
+(define (adjoin-tree-MD MD-record MD-tree)
+  (if (not (pair? (value MD-record)))
+      (adjoin-tree-1D MD-record MD-tree)
+      (let ((first-given-key (key MD-record))
+	    (subrecord (value MD-record)))
+	(if (null? MD-tree)
+	    (make-tree (cons first-given-key
+			     (adjoin-tree-MD subrecord '()))
+		       '()
+		       '())
+	    (let ((first-entry-key (key (entry MD-tree))))
+	      (cond ((= first-given-key first-entry-key)
+		     (make-tree (cons first-entry-key
+				      (adjoin-tree-MD subrecord (cdr (entry MD-tree))))
+				(left-branch MD-tree)
+				(right-branch MD-tree)))
+		    ((< first-given-key first-entry-key)
+		     (make-tree (entry MD-tree)
+				(adjoin-tree-MD MD-record (left-branch MD-tree))
+				(right-branch MD-tree)))
+		    (else
+		     (make-tree (entry MD-tree)
+				(left-branch MD-tree)
+				(adjoin-tree-MD MD-record (right-branch MD-tree))))))))))
+;Value: adjoin-tree-md
+
+(define (adjoin-tree-1D record tree)
+  (if (null? tree)
+      (make-tree record '() '())
+      (let ((given-key (key record))
+	    (entry-key (key (entry tree))))
+	(cond ((= given-key entry-key)
+	       (make-tree record
+			  (left-branch tree)
+			  (right-branch tree)))
+	      ((< given-key entry-key)
+	       (make-tree (entry tree)
+			  (adjoin-tree-1D record (left-branch tree))
+			  (right-branch tree)))
+	      (else
+	       (make-tree (entry tree)
+			  (left-branch tree)
+			  (adjoin-tree-1D record (right-branch tree))))))))
+;Value: adjoin-tree-1d
+
+(define (make-table)
+  (list '*table*))
+;Value: make-table
+
+;; Universities and Colleges
+
+(define Havard 001)
+;Value: havard
+
+(define MIT 002)
+;Value: mit
+
+(define Cambridge 003)
+;Value: cambridge
+
+(define Oxford 004)
+;Value: oxford
+
+(define Harvy-Mudd 005)
+;Value: harvy-mudd
+
+(define Stanford 006)
+;Value: stanford
+
+(define HKU 007)
+;Value: hku
+
+(define TokyoU 008)
+;Value: tokyou
+
+
+;; Stuff Rank
+
+(define Student 01)
+;Value: student
+
+(define Teacher 02)
+;Value: teacher
+
+(define President 03)
+;Value: president
+
+
+;; Schools and Departments
+
+(define Philosophy 01)
+;Value: philosophy
+
+(define Mathematics 02)
+;Value: mathematics
+
+(define Law 03)
+;Value: law
+
+(define Physics 04)
+;Value: physics
+
+(define Chemistry 05)
+;Value: chemistry
+
+(define EECS 06)
+;Value: eecs
+
+(define Biology 07)
+;Value: biology
+
+(define Mechanics 08)
+;Value: mechanics
+
+(define Bussiness 09)
+;Value: bussiness
+
+
+
+;; Students Rank
+
+(define Undergraduate 01)
+;Value: undergraduate
+
+(define Graduate 02)
+;Value: graduate
+
+
+
+;; Teacher Rank
+
+(define Lecturer 01)
+;Value: lecturer
+
+(define Assistant-Professor 02)
+;Value: assistant-professor
+
+(define Associate-Professor 03)
+;Value: associate-professor
+
+(define Professor 04)
+;Value: professor
+
+(define Tenure-Professor 05)
+;Value: tenure-professor
+
+
+
+
+(define T8 (make-table))
+;Value: t8
+
+(lookup (list MIT Student Undergraduate Mathematics 120) T8)
+;Value: #f
+
+(lookup (list MIT Student Undergraduate Biology 006) T8)
+;Value: #f
+
+(lookup (list MIT Student Graduate Mechanics 015) T8)
+;Value: #f
+
+(lookup (list MIT Teacher Tenure-Professor 024) T8)
+;Value: #f
+
+(lookup (list MIT Teacher Lecturer 064) T8)
+;Value: #f
+
+(lookup (list MIT President) T8)
+;Value: #f
+
+
+
+(lookup (list HKU Student Undergraduate EECS 051) T8)
+;Value: #f
+
+(lookup (list HKU Student Undergraduate Philosophy 012) T8)
+;Value: #f
+
+(lookup (list HKU Student Graduate Physics 075) T8)
+;Value: #f
+
+(lookup (list HKU Teacher Associate-Professor 003) T8)
+;Value: #f
+
+(lookup (list HKU Teacher Professor 025) T8)
+;Value: #f
+
+(lookup (list HKU President) T8)
+;Value: #f
+
+
+
+(insert! (list MIT Student Undergraduate Mathematics 120) 'Dorgan T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ()) () ()) () ()) () ())
+;Unspecified return value
+
+(insert! (list MIT Student Undergraduate Biology 006) 'Kermo T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ()) () ()) () ())
+;Unspecified return value
+
+(insert! (list MIT Student Graduate Mechanics 015) 'Graham T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ()) () ())
+;Unspecified return value
+
+
+(insert! (list MIT Teacher Tenure-Professor 024) 'Strang T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) () ()) () ())) () ())
+;Unspecified return value
+
+(insert! (list MIT Teacher Lecturer 064) 'Menlord T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) ((1 (64 . menlord) () ()) () ()) ()) () ())) () ())
+;Unspecified return value
+
+(insert! (list MIT President) 'Reif T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) ((1 (64 . menlord) () ()) () ()) ()) () ((3 . reif) () ()))) () ())
+;Unspecified return value
+
+(lookup (list MIT Student Undergraduate Mathematics 120) T8)
+;Value: dorgan
+
+(lookup (list MIT Student Undergraduate Biology 006) T8)
+;Value: kermo
+
+(lookup (list MIT Student Graduate Mechanics 015) T8)
+;Value: graham
+
+(lookup (list MIT Teacher Tenure-Professor 024) T8)
+;Value: strang
+
+(lookup (list MIT Teacher Lecturer 064) T8)
+;Value: menlord
+
+(lookup (list MIT President) T8)
+;Value: reif
+
+
+
+(insert! (list HKU Student Undergraduate EECS 051) 'Ding T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) ((1 (64 . menlord) () ()) () ()) ()) () ((3 . reif) () ()))) () ((7 (1 (1 (6 (51 . ding) () ()) () ()) () ()) () ()) () ()))
+;Unspecified return value
+
+
+(insert! (list HKU Student Undergraduate Philosophy 012) 'Yan T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) ((1 (64 . menlord) () ()) () ()) ()) () ((3 . reif) () ()))) () ((7 (1 (1 (6 (51 . ding) () ()) ((1 (12 . yan) () ()) () ()) ()) () ()) () ()) () ()))
+;Unspecified return value
+
+
+(insert! (list HKU Student Graduate Physics 075) 'Bai T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) ((1 (64 . menlord) () ()) () ()) ()) () ((3 . reif) () ()))) () ((7 (1 (1 (6 (51 . ding) () ()) ((1 (12 . yan) () ()) () ()) ()) () ((2 (4 (75 . bai) () ()) () ()) () ())) () ()) () ()))
+;Unspecified return value
+
+(insert! (list HKU Teacher Associate-Professor 003) 'Lin  T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) ((1 (64 . menlord) () ()) () ()) ()) () ((3 . reif) () ()))) () ((7 (1 (1 (6 (51 . ding) () ()) ((1 (12 . yan) () ()) () ()) ()) () ((2 (4 (75 . bai) () ()) () ()) () ())) () ((2 (3 (3 . lin) () ()) () ()) () ())) () ()))
+;Unspecified return value
+
+
+(insert! (list HKU Teacher Professor 025) 'Morgan T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) ((1 (64 . menlord) () ()) () ()) ()) () ((3 . reif) () ()))) () ((7 (1 (1 (6 (51 . ding) () ()) ((1 (12 . yan) () ()) () ()) ()) () ((2 (4 (75 . bai) () ()) () ()) () ())) () ((2 (3 (3 . lin) () ()) () ((4 (25 . morgan) () ()) () ())) () ())) () ()))
+;Unspecified return value
+
+
+(insert! (list HKU President) 'Cai T8)
+;Value: ok
+
+(display T8)
+(*table* (2 (1 (1 (2 (120 . dorgan) () ()) () ((7 (6 . kermo) () ()) () ())) () ((2 (8 (15 . graham) () ()) () ()) () ())) () ((2 (5 (24 . strang) () ()) ((1 (64 . menlord) () ()) () ()) ()) () ((3 . reif) () ()))) () ((7 (1 (1 (6 (51 . ding) () ()) ((1 (12 . yan) () ()) () ()) ()) () ((2 (4 (75 . bai) () ()) () ()) () ())) () ((2 (3 (3 . lin) () ()) () ((4 (25 . morgan) () ()) () ())) () ((3 . cai) () ()))) () ()))
+;Unspecified return value
+
+(lookup (list HKU Student Undergraduate EECS 051) T8)
+;Value: ding
+
+(lookup (list HKU Student Undergraduate Philosophy 012) T8)
+;Value: yan
+
+(lookup (list HKU Student Graduate Physics 075) T8)
+;Value: bai
+
+(lookup (list HKU Teacher Associate-Professor 003) T8)
+;Value: lin
+
+(lookup (list HKU Teacher Professor 025) T8)
+;Value: morgan
+
+(lookup (list HKU President) T8)
+;Value: cai
 

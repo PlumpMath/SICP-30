1d4054b34a9f2772f0e82e24cbe21b1705a7f20f
Lawrence R. Amlord <informlarry@gmail.com>
2013-10-10 00:52:52 +0800

    Add my answer to Exercise 3.29.
    Included are the following 3 files:
    
    	Exercise_3.29.pdf
    	Exercise_3.29.scm
    	Exercise_3.29.tm
    
    Thank you!
    							Lawrence X. Amlord
    							  Oct 10th, 2013
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_3/Exercise_3.29/Exercise_3.29.scm
@@ -0,0 +1,35 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_3.29.scm
+;;                       by Lawrence X. Amlord(颜序, aka 颜世敏)
+;;                       informlarry@gmail.com
+;;                       Oct 9th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence X. Amlord(颜序, aka 颜世敏)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (or-gate a1 a2 output)
+  (let ((b1 (make-wire))
+	(b2 (make-wire))
+	(c (make-wire)))
+    (inverter a1 b1)
+    (inverter a2 b2)
+    (and-gate b1 b2 c)
+    (inverter c output)
+    'ok))

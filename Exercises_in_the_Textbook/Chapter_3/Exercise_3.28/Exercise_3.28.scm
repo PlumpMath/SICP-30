dfc578d2ffd04ee137235c8c1c9d7516475daecc
Lawrence R. Amlord <informlarry@gmail.com>
2013-10-09 16:30:19 +0800

    Add my answer to Exercise 3.28.
    Included is the following file:
    
    	Exercise_3.28.scm
    
    Thank you!
    							Lawrence X. Amlord
    							  Oct 9th, 2013
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_3/Exercise_3.28/Exercise_3.28.scm
@@ -0,0 +1,45 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       (Put the name of the program here.)
+;;                       by Lawrence X. Amlord(颜序, aka 颜世敏)
+;;                       informlarry@gmail.com
+;;                       (Put the date here.)
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence X. Amlord(颜序, aka 颜世敏)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+	   (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-delay
+		   (lambda ()
+		     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+(define (logical-or s1 s2)
+  (cond ((and (= s1 0) (= s2 0)) 0)
+	((or (and (= s1 1) (= s2 1))
+	     (and (= s1 1) (= s2 0))
+	     (and (= s1 0) (= s2 1)))
+	 1)
+	(else (error "Invalid signal" s1 s2))))
+

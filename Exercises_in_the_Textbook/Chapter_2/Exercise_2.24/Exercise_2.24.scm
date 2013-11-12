b1259f09c669eefb831ae0d0ab0daab1886289a5
Lawrence R. Amlord <informlarry@gmail.com>
2013-11-12 15:13:39 +0800

    Update my answer to Exercise 2.24.
    This is a major release! It corrects the wrong solution in its predecessor.
    
    Modified are the following 3 files:
    
    	Exercise_2.24.pdf
    	Exercise_2.24.scm
    	Exercise_2.24.tm
    
    Thank you!
    							Lawrence X. Amlord
    							  Nov 12th, 2013
    							  Xi'an, China

--- Exercises_in_the_Textbook/Chapter_2/Exercise_2.24/Exercise_2.24.scm
+++ Exercises_in_the_Textbook/Chapter_2/Exercise_2.24/Exercise_2.24.scm
@@ -1,28 +1,36 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;                       Exercise_2.24.scm
-;;                       by Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;;                       Test_for_Exercise_2.24.scm
+;;                       by Lawrence X. Amlord(颜序, aka 颜世敏)
 ;;                       informlarry@gmail.com
 ;;                       May 29th, 2013
 ;;                       Xi'an, China
 
-;; Copyright (C) 2013 Lawrence R. Amlord(颜世敏 Shi-min Yan)
+;; Copyright (C) 2013 Lawrence X. Amlord(颜序, aka 颜世敏)
 ;; <informlarry@gmail.com>
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (list 1 (list 2 (list 3 4)))
-;Value 19: (1 (2 (3 4)))
+;Value 13: (1 (2 (3 4)))
+
+(cons 1
+      (cons (list 2 (list 3 4))
+	    '()))
+;Value 14: (1 (2 (3 4)))
+
+(cons 1
+      (cons (cons 2
+		  (cons (list 3 4)
+			'()))
+	    '()))
+;Value 15: (1 (2 (3 4)))
+
+(cons 1
+      (cons (cons 2
+		  (cons (cons 3
+			      (cons 4 '()))
+			'()))
+	    '()))
+;Value 16: (1 (2 (3 4)))
+

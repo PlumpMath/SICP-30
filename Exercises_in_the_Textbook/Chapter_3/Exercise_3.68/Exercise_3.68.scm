1ef400387154410d85f5993949cc28f21a9ff342
Lawrence R. Amlord <informlarry@gmail.com>
2013-12-04 01:33:49 +0800

    Add my answer to Exercise 3.68.
    Included are the following 3 files:
    
    	Exercise_3.68.pdf
    	Exercise_3.68.scm
    	Exercise_3.68.tm
    
    Thank you!
    							Lawrence X. Amlord
    							  Dec 4th, 2013
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_3/Exercise_3.68/Exercise_3.68.scm
@@ -0,0 +1,45 @@
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+	     (stream-for-each proc (stream-cdr s)))))
+;Value: stream-for-each
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+;Value: display-stream
+
+(define (display-line x)
+  (newline)
+  (display x))
+;Value: display-line
+
+(define ones (cons-stream 1 ones))
+;Value: ones
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+;Value: add-streams
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+;Value: integers
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+		   (interleave s2 (stream-cdr s1)))))
+;Value: interleave
+
+(define (pairs s t)
+  (interleave
+   (stream-map (lambda (x) (list (stream-car s) x))
+	       t)
+   (pairs (stream-cdr s) (stream-cdr t))))
+;Value: pairs
+
+(pairs integers integers)
+
+;Aborting!: maximum recursion depth exceeded
+
+

47b401ff8b9e799d919579e1235c3de59cd36d7f
Lawrence R. Amlord <informlarry@gmail.com>
2013-12-09 13:17:15 +0800

    Add my answer to Exercise 4.1.
    Included is the following file:
    
    	Exercise_4.1.scm
    
    Thank you!
    							Lawrence X. Amlord
    							  Dec 9th, 2013
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_4/Exercise_4.1/Exercise_4.1.scm
@@ -0,0 +1,39 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_4.1.scm
+;;                       by Lawrence X. Amlord(颜世敏, aka 颜序)
+;;                       informlarry@gmail.com
+;;                       Dec 9th, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 2013 Lawrence X. Amlord(颜世敏, aka 颜序)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (list-of-values-left-to-right exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((first-operand-value (eval (first-operand exps) env)))
+	(cons first-operand-value
+	      (list-of-values-left-to-right (rest-operands exps) env)))))
+
+(define (list-of-values-right-to-left exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((rest-operand-values (list-of-values-right-to-left (rest-operands exp) env)))
+	(cons (eval (first-operand exps) env)
+	      rest-operand-values))))

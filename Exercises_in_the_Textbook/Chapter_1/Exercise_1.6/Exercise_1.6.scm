dedbdfef318a875a3f53e59f0fe8eae560c2cbe5
Lawrence R. Amlord <informlarry@gmail.com>
2014-02-19 15:18:17 +0800

    Update my answer to Exercise 1.6.
    Modified are the following 3 files:
    
    	Exercise_1.6.pdf
    	Exercise_1.6.scm
    	Exercise_1.6.tm
    
    Thank you!
    							Lawrence X. Amlord
    							  Feb 19th, 2014
    							  Fuzhou, China

--- Exercises_in_the_Textbook/Chapter_1/Exercise_1.6/Exercise_1.6.scm
+++ Exercises_in_the_Textbook/Chapter_1/Exercise_1.6/Exercise_1.6.scm
@@ -1,29 +1,51 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_1.6.scm
+;;                       originated from SICP
+;;                       edited by Lawrence X. Amlord(颜世敏, aka 颜序)
+;;                       informlarry@gmail.com
+;;                       Apr 23rd, 2013
+;;                       Xi'an, China
+
+;; Copyright (C) 1984-2013 Harold Abelson and Gerald Jay Sussman
+;; with Julie Sussman
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define (new-if predicate then-clause else-clause)
   (cond (predicate then-clause)
         (else else-clause)))
+;Value: new-if
 
 (new-if (= 2 3) 0 5)
+;Value: 5
 
 (new-if (= 1 1) 0 5)
-
-;; Auxiliary Procedures
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+;Value: 0
 
 (define (square x)
   (* x x))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
+;Value: square
 
 (define (average x y)
   (/ (+ x y) 2))
+;Value: average
 
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+;Value: good-enough?
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+;Value: improve
 
 (define (sqrt-iter guess x)
   (new-if (good-enough? guess x)
           guess
           (sqrt-iter (improve guess x)
                      x)))
+;Value: sqrt-iter
+(sqrt 2)
 
-(sqrt-iter 1.0 25)
+;Aborting!: maximum recursion depth exceeded
+

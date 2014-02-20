b911774f97396a10f512a8ad3af6af53660e5292
Lawrence R. Amlord <informlarry@gmail.com>
2014-02-20 22:11:33 +0800

    Implement The Lazy Evaluator.
    Included are the following 2 files:
    
    	Lazy_Evaluator.scm
    	Test_for_Lazy_Evaluator.scm
    
    Thank you!
    							Lawrence X. Amlord
    							  Feb 20th, 2014
    							  Fuzhou, China

---
+++ Examples_to_Practice/Chapter_4/4.2_Variations_on_a_Scheme--Lazy_Evaluation/4.2.2_An_Interpreter_with_Lazy_Evaluation/Test_for_Lazy_Evaluator.scm
@@ -0,0 +1,212 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Lazy_Evaluator.scm
+;;                       originated from SICP
+;;                       edited by Lawrence X. Amlord(颜世敏, aka 颜序)
+;;                       informlarry@gmail.com
+;;                       Feb 20th, 2014
+;;                       Fuzhou, China
+
+;; Copyright (C) 1984-2014 Harold Abelson and Gerald Jay Sussman
+;; with Julie Sussman
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define apply-in-underlying-scheme apply)
+;Value: apply-in-underlying-scheme
+
+;Value: actual-value
+
+;Value: eval
+
+;Value: apply
+
+;Value: list-of-arg-values
+
+;Value: list-of-delayed-args
+
+;Value: eval-if
+
+;Value: eval-sequence
+
+;Value: eval-assignment
+
+;Value: eval-definition
+
+;Value: self-evaluating?
+
+;Value: variable?
+
+;Value: quoted?
+
+;Value: text-of-quotation
+
+;Value: tagged-list?
+
+;Value: assignment?
+
+;Value: assignment-variable
+
+;Value: assignment-value
+
+;Value: definition?
+
+;Value: definition-variable
+
+;Value: definition-value
+
+;Value: lambda?
+
+;Value: lambda-parameters
+
+;Value: lambda-body
+
+;Value: make-lambda
+
+;Value: if?
+
+;Value: if-predicate
+
+;Value: if-consequent
+
+;Value: if-alternative
+
+;Value: make-if
+
+;Value: begin?
+
+;Value: begin-actions
+
+;Value: last-exp?
+
+;Value: first-exp
+
+;Value: rest-exps
+
+;Value: sequence->exp
+
+;Value: make-begin
+
+;Value: application?
+
+;Value: operator
+
+;Value: operands
+
+;Value: no-operands?
+
+;Value: first-operand
+
+;Value: rest-operands
+
+;Value: cond?
+
+;Value: cond-clauses
+
+;Value: cond-else-clause?
+
+;Value: cond-predicate
+
+;Value: cond-actions
+
+;Value: cond->if
+
+;Value: expand-clauses
+
+;Value: true?
+
+;Value: false?
+
+;Value: make-procedure
+
+;Value: compound-procedure?
+
+;Value: procedure-parameters
+
+;Value: procedure-body
+
+;Value: procedure-environment
+
+;Value: enclosing-environment
+
+;Value: first-frame
+
+;Value: the-empty-environment
+
+;Value: make-frame
+
+;Value: frame-variables
+
+;Value: frame-values
+
+;Value: add-binding-to-frame!
+
+;Value: extend-environment
+
+;Value: lookup-variable-value
+
+;Value: set-variable-value!
+
+;Value: define-variable!
+
+;Value: delay-it
+
+;Value: thunk?
+
+;Value: thunk-exp
+
+;Value: thunk-env
+
+;Value: evaluated-thunk?
+
+;Value: thunk-value
+
+;Value: force-it
+
+;Value: setup-environment
+
+;Value: primitive-procedure?
+
+;Value: primitive-implementation
+
+;Value: primitive-procedures
+
+;Value: primitive-procedure-names
+
+;Value: primitive-procedure-objects
+
+;Value: apply-primitive-procedure
+
+;Value: input-prompt
+
+;Value: output-prompt
+
+;Value: driver-loop
+
+;Value: prompt-for-input
+
+;Value: announce-output
+
+;Value: user-print
+
+;Value: the-global-environment
+
+(driver-loop)
+
+
+;;; L-Eval input:
+(define (try a b)
+  (if (= a 0) 1 b))
+
+;;; L-Eval value:
+ok
+
+;;; L-Eval input:
+(try 0 (/ 1 0))
+
+;;; L-Eval value:
+1
+
+;;; L-Eval input:
+;Quit!
+

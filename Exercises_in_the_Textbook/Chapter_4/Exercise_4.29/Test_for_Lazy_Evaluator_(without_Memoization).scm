5026decc84dad483e49a258a92fd0a50c7a9a7b0
Lawrence R. Amlord <informlarry@gmail.com>
2014-02-24 15:46:41 +0800

    Add my answer to Exercise 4.29.
    Included are the following 6 files:
    
    	Exercise_4.29.pdf
    	Exercise_4.29.tm
    	Lazy_Evaluator_(with_Memoization).scm
    	Lazy_Evaluator_(without_Memoization).scm
    	Test_for_Lazy_Evaluator_(with_Memoization).scm
    	Test_for_Lazy_Evaluator_(without_Memoization).scm
    
    Thank you!
    							Lawrence X. Amlord
    							  Feb 24th, 2014
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_4/Exercise_4.29/Test_for_Lazy_Evaluator_(without_Memoization).scm
@@ -0,0 +1,275 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Test_for_Lazy_Evaluator_(without_Memoization).scm
+;;                       by Lawrence X. Amlord(颜世敏, aka 颜序)
+;;                       informlarry@gmail.com
+;;                       Feb 23rd, 2014
+;;                       Xi'an, China
+
+;; Copyright (C) 2014 Lawrence X. Amlord(颜世敏, aka 颜序)
+;; <informlarry@gmail.com>
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define apply-in-underlying-scheme apply)
+;Value: apply-in-underlying-scheme
+
+;Value: actual-value
+
+;Value: eval
+
+;Value: apply
+
+;Value: list-of-arg-values
+
+;Value: list-of-delayed-args
+
+;Value: eval-if
+
+;Value: eval-sequence
+
+;Value: eval-assignment
+
+;Value: eval-definition
+
+;Value: self-evaluating?
+
+;Value: variable?
+
+;Value: quoted?
+
+;Value: text-of-quotation
+
+;Value: tagged-list?
+
+;Value: assignment?
+
+;Value: assignment-variable
+
+;Value: assignment-value
+
+;Value: definition?
+
+;Value: definition-variable
+
+;Value: definition-value
+
+;Value: lambda?
+
+;Value: lambda-parameters
+
+;Value: lambda-body
+
+;Value: make-lambda
+
+;Value: if?
+
+;Value: if-predicate
+
+;Value: if-consequent
+
+;Value: if-alternative
+
+;Value: make-if
+
+;Value: begin?
+
+;Value: begin-actions
+
+;Value: last-exp?
+
+;Value: first-exp
+
+;Value: rest-exps
+
+;Value: sequence->exp
+
+;Value: make-begin
+
+;Value: application?
+
+;Value: operator
+
+;Value: operands
+
+;Value: no-operands?
+
+;Value: first-operand
+
+;Value: rest-operands
+
+;Value: cond?
+
+;Value: cond-clauses
+
+;Value: cond-else-clause?
+
+;Value: cond-predicate
+
+;Value: cond-actions
+
+;Value: cond->if
+
+;Value: expand-clauses
+
+;Value: true?
+
+;Value: false?
+
+;Value: make-procedure
+
+;Value: compound-procedure?
+
+;Value: procedure-parameters
+
+;Value: procedure-body
+
+;Value: procedure-environment
+
+;Value: enclosing-environment
+
+;Value: first-frame
+
+;Value: the-empty-environment
+
+;Value: make-frame
+
+;Value: frame-variables
+
+;Value: frame-values
+
+;Value: add-binding-to-frame!
+
+;Value: extend-environment
+
+;Value: lookup-variable-value
+
+;Value: set-variable-value!
+
+;Value: define-variable!
+
+;Value: delay-it
+
+;Value: thunk?
+
+;Value: thunk-exp
+
+;Value: thunk-env
+
+;Value: force-it
+
+;Value: setup-environment
+
+;Value: primitive-procedure?
+
+;Value: primitive-implementation
+
+;Value: primitive-procedures
+
+;Value: primitive-procedure-names
+
+;Value: primitive-procedure-objects
+
+;Value: apply-primitive-procedure
+
+;Value: input-prompt
+
+;Value: output-prompt
+
+;Value: time-prompt
+
+;Value: driver-loop
+
+;Value: prompt-for-input
+
+;Value: announce-output
+
+;Value: user-print
+
+;Value: the-global-environment
+
+(driver-loop)
+
+
+;;; L-Eval input:
+(define (fib n)
+  (cond ((= n 0) 1)
+	((= n 1) 1)
+	(else (+ (fib (- n 1))
+		 (fib (- n 2))))))
+
+;;; L-Eval value:
+ok
+;;; Elapsed time:
+0.
+
+;;; L-Eval input:
+(fib 10)
+
+;;; L-Eval value:
+89
+;;; Elapsed time:
+.13
+
+;;; L-Eval input:
+(fib 16)
+
+;;; L-Eval value:
+1597
+;;; Elapsed time:
+3.2800000000000002
+
+;;; L-Eval input:
+(fib 20)
+
+;;; L-Eval value:
+10946
+;;; Elapsed time:
+27.48
+
+;;; L-Eval input:
+(define count 0)
+
+;;; L-Eval value:
+ok
+;;; Elapsed time:
+0.
+
+;;; L-Eval input:
+(define (id x)
+  (set! count (+ count 1))
+  x)
+
+;;; L-Eval value:
+ok
+;;; Elapsed time:
+0.
+
+;;; L-Eval input:
+(define (square x)
+  (* x x))
+
+;;; L-Eval value:
+ok
+;;; Elapsed time:
+0.
+
+;;; L-Eval input:
+(square (id 10))
+
+;;; L-Eval value:
+100
+;;; Elapsed time:
+0.
+
+;;; L-Eval input:
+count
+
+;;; L-Eval value:
+2
+;;; Elapsed time:
+0.
+
+;;; L-Eval input:
+;Quit!
+

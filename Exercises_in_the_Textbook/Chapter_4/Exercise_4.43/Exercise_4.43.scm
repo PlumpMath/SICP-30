6ead178193256c43ebfec4faade072036b2d34f1
Lawrence R. Amlord <informlarry@gmail.com>
2014-03-07 15:27:15 +0800

    Add my answer to Exercise 4.43.
    Included are the following 3 files:
    
    	Exercise_4.43.scm
    	ch4-ambeval.scm
    	ch4-mceval.scm
    
    Thank you!
    							Lawrence X. Amlord
    							  Mar 7th, 2014
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_4/Exercise_4.43/Exercise_4.43.scm
@@ -0,0 +1,157 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_4.43.scm
+;;                       by Lawrence X. Amlord(颜世敏, aka 颜序)
+;;                       informlarry@gmail.com
+;;                       Mar 6th, 2014
+;;                       Xi'an, China
+
+;; Copyright (C) 2014 Lawrence X. Amlord(颜世敏, aka 颜序)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(load "/Users/Lawrence/Documents/CSAIL/SICP_Journey/Exercises_in_the_Textbook/Chapter_4/Exercise_4.43/ch4-ambeval.scm")
+
+;Loading "ch4-ambeval.scm"...
+;  Loading "ch4-mceval.scm"... done
+;... done
+;Value: amb-evaluator-loaded
+
+(define the-global-environment (setup-environment))
+;Value: the-global-environment
+
+(driver-loop)
+
+
+;;; Amb-Eval input:
+(define (require p)
+  (if (not p) (amb)))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+ok
+
+;;; Amb-Eval input:
+(define (distinct? items)
+  (cond ((null? items) true)
+	((null? (cdr items)) true)
+	((member (car items) (cdr items)) false)
+	(else (distinct? (cdr items)))))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+ok
+
+;;; Amb-Eval input:
+(define (make-family father daughter yacht)
+  (list father daughter yacht))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+ok
+
+;;; Amb-Eval input:
+(define (father family)
+  (car family))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+ok
+
+;;; Amb-Eval input:
+(define (daughter family)
+  (car (cdr family)))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+ok
+
+;;; Amb-Eval input:
+(define (yacht family)
+  (car (cdr (cdr family))))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+ok
+
+;;; Amb-Eval input:
+(define (family-matching)
+  (let ((moore-family
+	 (make-family 'moore
+		      'mary
+		      'lorna))
+	(downing-family
+	 (make-family 'downing
+		      (amb 'gabrielle 'lorna 'rosalind)
+		      'melissa))
+	(hall-family
+	 (make-family 'hall
+		      (amb 'gabrielle 'lorna)
+		      'rosalind))
+	(barnacle-family
+	 (make-family 'barnacle
+		      'melissa
+		      'gabrielle))
+	(parker-family
+	 (make-family 'parker
+		      (amb 'lorna 'rosalind)
+		      'mary)))
+    (let ((families (list moore-family
+			  downing-family
+			  hall-family
+			  barnacle-family
+			  parker-family)))
+      (require
+       (distinct? (daughter downing-family)
+		  (daughter hall-family)
+		  (daughter parker-family)))
+      (require
+       (eq? (yacht (filter (lambda (family)
+			     (eq? (daughter family) 'gabrielle))
+			   families))
+	    (daughter parker-family)))
+      (list (list 'moore 'mary)
+	    (list 'downing (daughter downing-family))
+	    (list 'hall (daughter hall-family))
+	    (list 'barnacle 'melissa)
+	    (list 'parker (daughter parker-family))))))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+ok
+
+;;; Amb-Eval input:
+(define (filter predicate items)
+  (cond ((null? items) '())
+	((predicate (car items))
+	 (cons (car items)
+	       (filter predicate (cdr items))))
+	(else (filter predicate (cdr items)))))
+
+;;; Starting a new problem 
+;;; Amb-Eval value:
+ok
+
+;;; Amb-Eval input:
+(family-matching)
+
+;;; Starting a new problem 
+;Unknown procedure type -- APPLY #[compiled-procedure 16 ("list" #x9) #x14 #x103cb737c]
+;To continue, call RESTART with an option number:
+; (RESTART 1) => Return to read-eval-print level 1.
+
+(debug)

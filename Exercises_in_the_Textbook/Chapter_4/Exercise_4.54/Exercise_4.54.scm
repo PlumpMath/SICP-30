05e226bc5f7e839080a55a71723e78a81d36075e
Lawrence R. Amlord <informlarry@gmail.com>
2014-03-18 19:44:03 +0800

    Add my answer to Exercise 4.54.
    Included are the following 2 files:
    
    	Exercise_4.54.scm
    	Test_for_Exercise_4.54.scm
    
    Thank you!
    							Lawrence X. Amlord
    							  Mar 18th, 2014
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_4/Exercise_4.54/Exercise_4.54.scm
@@ -0,0 +1,35 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_4.54.scm
+;;                       by Lawrence X. Amlord(颜世敏, aka 颜序)
+;;                       informlarry@gmail.com
+;;                       Mar 
+;;                       Xi'an, China
+
+;; Copyright (C) 2014 Lawrence X. Amlord(颜世敏, aka 颜序)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (analyze-require exp)
+  (let ((pproc (analyze (require-predicate exp))))
+    (lambda (env succeed fail)
+      (pproc env
+	     (lambda (pred-value fail2)
+	       (if (false? pred-value)
+		   (fail2)
+		   (succeed 'ok fail2)))
+	     fail))))

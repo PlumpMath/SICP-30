309d49cd86b9fcdbbd2d872ff18b931346da005a
Lawrence R. Amlord <informlarry@gmail.com>
2014-03-21 21:40:09 +0800

    Add my answer to Exercise 4.55.
    Included are the following 3 files:
    
    	Exercise_4.55.pdf
    	Exercise_4.55.scm
    	Exercise_4.55.tm
    
    Thank you!
    							Lawrence X. Amlord
    							  Mar 21st, 2014
    							  Xi'an, China

---
+++ Exercises_in_the_Textbook/Chapter_4/Exercise_4.55/Exercise_4.55.scm
@@ -0,0 +1,34 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;                       Exercise_4.55.scm
+;;                       by Lawrence X. Amlord(颜世敏, aka 颜序)
+;;                       informlarry@gmail.com
+;;                       Mar 21st, 2014
+;;                       Xi'an, China
+
+;; Copyright (C) 2014 Lawrence X. Amlord(颜世敏, aka 颜序)
+;; <informlarry@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; part a.
+(supervisor ?x (Bitdiddle Ben))
+
+;; part b.
+(job ?x (accounting . ?type))
+
+;; part c.
+(address ?x (Slumerville . ?type))

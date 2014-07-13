d09cfaf460dc1c1d32642816812be94333cd7584
Lawrence R. Amlord <informlarry@gmail.com>
2014-07-13 11:41:12 +0800

    verified solution to Exercise 4.55.
    Modified is the following file:
    
    	Exercise_4.55.scm
    
    redundant file is removed:
    
    	ch4-query.scm
    
    Thank you!
    							Lawrence X. Amlord
    							  Jul 13th, 2014
    							  Xi'an, China

--- Exercises_in_the_Textbook/Chapter_4/Exercise_4.55/Exercise_4.55.scm
+++ Exercises_in_the_Textbook/Chapter_4/Exercise_4.55/Exercise_4.55.scm
@@ -24,11 +24,38 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; part a.
+(load "/Users/Lawrence/Campus/Engineering/EECS/SICP/allcode/ch4-query.scm")
+
+;Loading "Campus/Engineering/EECS/SICP/allcode/ch4-query.scm"... done
+;Value: microshaft-data-base
+
+(initialize-data-base microshaft-data-base)
+;Value: done
+
+(query-driver-loop)
+
+
+;;; Query input:
 (supervisor ?x (Bitdiddle Ben))
 
-;; part b.
+;;; Query results:
+(supervisor (tweakit lem e) (bitdiddle ben))
+(supervisor (fect cy d) (bitdiddle ben))
+(supervisor (hacker alyssa p) (bitdiddle ben))
+
+;;; Query input:
 (job ?x (accounting . ?type))
 
-;; part c.
+;;; Query results:
+(job (cratchet robert) (accounting scrivener))
+(job (scrooge eben) (accounting chief accountant))
+
+;;; Query input:
 (address ?x (Slumerville . ?type))
+
+;;; Query results:
+(address (aull dewitt) (slumerville (onion square) 5))
+(address (reasoner louis) (slumerville (pine tree road) 80))
+(address (bitdiddle ben) (slumerville (ridge road) 10))
+
+;;; Query input:
